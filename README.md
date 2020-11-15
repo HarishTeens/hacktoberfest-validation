a367eefc652730bcbff686beaaeffbbbaeff0924
HarishTeens <arishh2@gmail.com>
2020-11-15 15:51:20 +0530

    add Readme

---
+++ README.md
@@ -0,0 +1,11 @@
+### Introduction
+
+An API inteded to simulate DigitalOcean's Hacktoberfest Validation Check.
+
+### Step to Use the API
+
+- Make a POST Request to ```http://localhost:4000/hacktoberfest``` .
+- Send your PAT(Personal Access Token) in Body.  Check this [link](https://github.com/settings/tokens) to get your PAT.
+
+### Issues
+- Add support for the PRs made before updating  guidelines.
--- app.js
+++ app.js
@@ -12,10 +12,9 @@ express.get("/",(req,res)=>{
     res.send("Magic lies underneath!")
 })
 
-express.post("/hacktoberfest",async (req,res)=>{
-    const userNodeID=req.body.id;
+express.post("/hacktoberfest",async (req,res)=>{    
     const userToken=req.body.token;
-    // const userNodeID=await fetchNodeID(userToken);
+    const userNodeID=await fetchNodeID(userToken);
     const PRHistory= await fetchPRHistory(userNodeID,userToken);
     const transformedPRs=await transformPRs(PRHistory);
     res.json(transformedPRs);
